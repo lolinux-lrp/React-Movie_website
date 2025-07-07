c65946e34bc703ae05a9d417514a12eff5936e4a
lolinux-lrp <lolinux.lrp@gmail.com>
2025-07-07 12:04:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>App Yay</div>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/React-Movie_website/',  // ← same as your repo name
+  plugins: [react()],
+})
