cc330558aab83003203febadfd6100f4a775dec8
lolinux-lrp <lolinux.lrp@gmail.com>
2025-07-23 10:46:38 +0530

    feat: Build the complete website

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   //base: '/React-Movie_website/',  // ← same as your repo name
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 })
